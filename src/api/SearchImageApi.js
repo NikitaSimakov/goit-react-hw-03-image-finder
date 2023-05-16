e6733409557da11cb18e5a61c538a35d6ae40a78
NikitaSimakov <mishaaniskin@gmail.com>
2023-05-16 22:47:08 +0300

    Update SearchImageApi.js

--- src/api/SearchImageApi.js
+++ src/api/SearchImageApi.js
@@ -1,5 +1,3 @@
-// import React, { Component } from 'react';
-
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '34692070-b05e0f04a1e41f744928729e6';
 
@@ -10,5 +8,3 @@ export const getSearchImages = async (searchQuery, page) => {
   const data = await response.json();
   return data;
 };
-
-// https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
